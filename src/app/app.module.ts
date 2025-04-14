0c59ae50f504ccec57cf8ffcc5655675fcec277b
azu <azu.santos.ete@gmail.com>
2025-04-14 17:15:07 +0200

    DataGrid_01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './lista/lista.component';
+import { DxDataGridModule } from 'devextreme-angular';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxDataGridModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DxDataGridComponent } from 'devextreme-angular';
+import { DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import ArrayStore from 'devextreme/data/array_store';
+import DataSource from 'devextreme/data/data_source';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.scss'],
+})
+export class ListaComponent implements OnInit {
+  @ViewChild('lista') datagrid!: DxDataGridComponent;
+
+  listaComidas = comidas;
+
+  model: any = {};
+
+  editOnkeyPress = true;
+
+  enterKeyAction: DxDataGridTypes.EnterKeyAction = 'moveFocus';
+
+  enterKeyDirection: DxDataGridTypes.EnterKeyDirection = 'row';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addFood() {
+    //Crear nueva comida con valores del model (from)
+    if(this.model.id!=undefined){
+      const newFood: Comida = {
+        id: this.model.id,
+        nombre: this.model.nombre,
+        descripcion: this.model.descripcion,
+        precio: this.model.precio,
+        saludable: this.model.saludable,
+      };
+      //Añadimos a la lista de comidas
+      this.listaComidas.push(newFood);
+      this.datagrid.instance.refresh();
+    }else{
+      alert("Debes especificar un id!");
+    }
+    
+  }
+
+  deleteFood() {
+    const idDelete = this.model.idDelete;
+    for (let i = 0; i < this.listaComidas.length; i++) {
+      if (this.listaComidas[i].id == idDelete) {
+        this.listaComidas.splice(i, 1);
+        break;
+      }
+    }
+    this.datagrid.instance.refresh();
+  }
+}
+
+class Comida {
+  id!: number;
+  nombre?: string;
+  descripcion?: string;
+  precio?: number;
+  saludable?: boolean;
+}
+
+var comidas: Comida[] = [
+  {
+    id: 1,
+    nombre: 'Patatas',
+    descripcion: 'Patatas condimentadas con diferentes especias',
+    precio: 5,
+    saludable: false,
+  },
+  {
+    id: 2,
+    nombre: 'Ensalada',
+    descripcion: 'Variado de verduras y legumbres frescas',
+    precio: 7,
+    saludable: true,
+  },
+  {
+    id: 3,
+    nombre: 'Bocadillo',
+    descripcion: 'Pan de masa madre con fiambre variado',
+    precio: 5,
+    saludable: true,
+  },
+  {
+    id: 4,
+    nombre: 'Plato combinado',
+    descripcion: 'Plato que depende del menú del día',
+    precio: 10,
+    saludable: false,
+  },
+];
